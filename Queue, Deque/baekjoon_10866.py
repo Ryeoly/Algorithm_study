762d496a988f88c6cc34b8b95adccff47a87179e
Ryeoly <55642147+Ryeoly@users.noreply.github.com>
2021-09-28 20:54:45 +0900

    deque

---
+++ Queue, Deque/baekjoon_10866.py
@@ -0,0 +1,41 @@
+from collections import deque
+import sys
+
+n = int(sys.stdin.readline())
+queue = deque()
+
+for i in range(n):
+    line = list(map(str, sys.stdin.readline().split()))
+    if line[0] == "push_front":
+        queue.appendleft(line[1])
+    elif line[0] == "push_back":
+        queue.append(line[1])
+    elif line[0] == "pop_front":
+        if len(queue) != 0:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif line[0] == "pop_back":
+        if len(queue) != 0:
+            print(queue.pop())
+        else:
+            print(-1)
+    elif line[0] == "size":
+        print(len(queue))
+    elif line[0] == "empty":
+        if len(queue) != 0:
+            print(0)
+        else:
+            print(1)
+    elif line[0] == "front":
+        if len(queue) != 0:
+            print(queue[0])
+        else:
+            print(-1)
+    elif line[0] == "back":
+        if len(queue) != 0:
+            print(queue[len(queue)-1])
+        else:
+            print(-1)
+    else:
+        print("error")
