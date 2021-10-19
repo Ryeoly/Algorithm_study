b65c903521ec1a657a5cd777eb7e2395fc8e8d45
Ryeoly <55642147+Ryeoly@users.noreply.github.com>
2021-10-19 17:04:25 +0900

    sort

---
+++ Sorting/baekjoon_1181.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = set()
+
+for i in range(n):
+    arr.add(sys.stdin.readline().rstrip())
+
+arr = list(arr)
+arr.sort(key=lambda arr:(len(arr), arr))
+
+for i in arr:
+    print(i)
