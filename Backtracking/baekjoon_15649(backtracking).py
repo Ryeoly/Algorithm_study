87f727f6c350f21b5c65b3527d8fa9cbb8c4ab3b
Ryeoly <55642147+Ryeoly@users.noreply.github.com>
2021-11-09 14:21:47 +0900

    dfs 일종 백트래킹

--- Backtracking/baekjoon_15649(backtracking).py
+++ Backtracking/baekjoon_15649(backtracking).py
@@ -3,10 +3,10 @@ s = []
 
 def dfs():
     if len(s) == m:
-        print(" ".join(map(str,s)))
+        print(" ".join(map(str, s)))
         return
 
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         if i not in s:
             s.append(i)
             dfs()
