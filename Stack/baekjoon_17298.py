676f9e37a164ecfd282023628767288c6941f549
Ryeoly <55642147+Ryeoly@users.noreply.github.com>
2021-12-07 16:22:59 +0900

    !어려운 개념!(코드 정리)

--- Stack/baekjoon_17298.py
+++ Stack/baekjoon_17298.py
@@ -7,14 +7,18 @@ stack.append((arr[0],0))
 answer = [-1] * n
 
 for i in range(1, n):
-    if stack[-1][0] > arr[i]:
-        stack.append((arr[i],i))
-    else:
-        while stack and stack[-1][0] < arr[i]:
-            temp = stack.pop()
-            answer[temp[1]] = arr[i]
-        stack.append((arr[i], i))
+    # if stack[-1][0] > arr[i]:
+    #     stack.append((arr[i],i))
+    # else:
+    #     while stack and stack[-1][0] < arr[i]:
+    #         temp = stack.pop()
+    #         answer[temp[1]] = arr[i]
+    #     stack.append((arr[i], i))
+    while stack and stack[-1][0] < arr[i]:
+        temp = stack.pop()
+        answer[temp[1]] = arr[i]
+    stack.append((arr[i], i))
 
-print(" ".join(list(map(str,answer))))
+print(" ".join(map(str,answer)))
 
 # 사진 자료를 확인
