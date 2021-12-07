9df3d91d72d6e94e396c9e4541530f2387c8094e
Ryeoly <55642147+Ryeoly@users.noreply.github.com>
2021-12-07 16:19:01 +0900

    !stack 개념을 활요하지 못했었음!

---
+++ Stack/baekjoon_17298.py
@@ -0,0 +1,20 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+stack = list()
+stack.append((arr[0],0))
+answer = [-1] * n
+
+for i in range(1, n):
+    if stack[-1][0] > arr[i]:
+        stack.append((arr[i],i))
+    else:
+        while stack and stack[-1][0] < arr[i]:
+            temp = stack.pop()
+            answer[temp[1]] = arr[i]
+        stack.append((arr[i], i))
+
+print(" ".join(list(map(str,answer))))
+
+# 사진 자료를 확인
