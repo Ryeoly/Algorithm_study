4c252b04c5b41f30e1169ae55ed4a18be7751cc9
Ryeoly <55642147+Ryeoly@users.noreply.github.com>
2021-12-09 14:55:01 +0900

    basic bfs

--- BFS, DFS/programmers_DFS,BFS_wordtrans.py
+++ BFS, DFS/programmers_DFS,BFS_wordtrans.py
@@ -1,33 +1,39 @@
+from collections import deque
+
 def count(word, want):
     count = 0
     for i in range(len(word)):
         if word[i] != want[i]:
             count += 1
-
     return count
-
 
 def solution(begin, target, words):
     answer = 0
-    a = words.index(target)
-    pos = 0
-    while pos < len(words):
-        dif = count(begin, target)
-        if dif == 1:
-            answer += 1
+
+    try:
+        words.index(target)
+    except:
+        return 0
+
+    arr = deque()
+    arr.append((begin, begin, 0))
+
+    while arr:
+        pre_word, cur_word, counting = arr.popleft()
+        if cur_word == target:
+            answer = counting
             break
-        trans_dif = count(begin, words[pos])
-        if trans_dif != 1:
-            return 0
-        else:
-            begin = words[pos]
-            pos += 1
-            answer += 1
 
+        for i in words:
+            if i == cur_word or i == pre_word:
+                continue
+            else:
+                dif = count(i, cur_word)
+                if dif == 1:
+                    arr.append((cur_word, i, counting+1))
     return answer
-
 
 begin = "hit"
 target = "cog"
-words = ["hot", "dot", "dog", "lot", "log"]
+words = ["hot", "dot", "dog", "lot", "log","cog"]
 print(solution(begin, target, words))
