16f5442272c4145101b254af3211a4f1f566459a
Ryeoly <55642147+Ryeoly@users.noreply.github.com>
2021-12-13 15:05:33 +0900

    최소 힙(heapq) 이용

---
+++ Implementation/baekjoon_1927.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+arr = list()
+for i in range(n):
+    x = int(sys.stdin.readline())
+    if x > 0:
+        heapq.heappush(arr, x)
+    else:
+        if len(arr) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(arr))
